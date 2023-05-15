37cc22acb8a0f759d824713e931051b22c2836ad
luisbarajasr <luis.br02@hotmail.com>
2023-05-15 08:25:54 -0600

    Correcciones, 8:25

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Login />} />
           <Route exact path='/cuestionario' element={<Cuestionario />} />
-      </Routes>
+        </Routes>
       </div>
 
   );
