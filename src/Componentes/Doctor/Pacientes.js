f764984468b317795fee38207b350f36d306ac49
luisbarajasr <luis.br02@hotmail.com>
2023-05-16 14:56:39 -0600

    Doctor View

---
+++ src/Componentes/Doctor/Pacientes.js
@@ -0,0 +1,53 @@
+export const Pacientes = [ 
+    {
+    "id" : 1, 
+    "nombre" : "Luis",
+    "apellido" : "Barajas"
+    }, 
+    {
+        "id" : 2, 
+        "nombre" : "John",
+        "apellido" : "Wick"
+    }, 
+    {
+        "id" : 3, 
+        "nombre" : "Camilo",
+        "apellido" : "Perez"
+    }, 
+    {
+        "id" : 4, 
+        "nombre" : "Isabella ",
+        "apellido" : "Justo"
+    }, 
+    {
+        "id" : 5, 
+        "nombre" : "Estefany",
+        "apellido" : "Morales"
+    }, 
+    {
+        "id" : 6, 
+        "nombre" : "Diego",
+        "apellido" : "Jimenez"
+    }, 
+    {
+        "id" : 7, 
+        "nombre" : "Gabriela",
+        "apellido" : "Suarez"
+    }, 
+    {
+        "id" : 8, 
+        "nombre" : "Danny",
+        "apellido" : "Fantom"
+        
+    },
+    {
+        "id" : 9, 
+        "nombre" : "Diego",
+        "apellido" : "Ramirez"
+    }, 
+    {
+        "id" : 10, 
+        "nombre" : "Anelise",
+        "apellido" : "Moreno"
+    }  
+]
