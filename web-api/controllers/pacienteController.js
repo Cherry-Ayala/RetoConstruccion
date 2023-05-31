d7946673e9f18dff05be16541b54f7bda7bbc1f7
ChalkyMuffin <95390155+ChalkyMuffin@users.noreply.github.com>
2023-05-31 12:51:15 -0600

    Busca paciente por ID e intento de agregar paciente

--- web-api/controllers/pacienteController.js
+++ web-api/controllers/pacienteController.js
@@ -15,6 +15,40 @@ class MainController {
         res.send(error.message)
       }
     }
+
+    async getPaciente(req , res){
+        try {
+            const pool = await poolPromise
+            const result = await pool.request()
+            .input('Id_Paciente',sql.Int, req.params.Id_Paciente)
+            .query("select * from [dbo].[Pacientes] where Id_Paciente = @Id_Paciente")
+            res.json(result.recordset)
+        } catch (error) {
+            res.status(500)
+            res.send(error.message)
+        }
+    }
+
+    async addPaciente(req , res){
+        try {
+          if(req.body.Nombre != null && req.body.Apellido_Paterno != null && req.body.Apellido_Materno != null && req.body.Fecha_Nacimiento != null) {
+            const pool = await poolPromise
+            const result = await pool.request()
+            .input('Nombre',sql.VarChar, req.body.Nombre)
+            .input('Apellido_Paterno',sql.VarChar, req.body.Apellido_Paterno)
+            .input('Apellido_Materno',sql.VarChar, req.body.Apellido_Materno)
+            .input('Fecha_Nacimiento',sql.Date, req.body.Fecha_Nacimiento)
+
+            .query("insert into [dbo].[Pacientes] values(@Nombre, @Apellido_Paterno, @Apellido_Materno, @Fecha_Nacimiento)")
+            res.json(result)
+          } else {
+            res.send('Por favor llena todos los datos!')
+          }
+        } catch (error) {
+          res.status(500)
+          res.send(error.message)
+        }
+      }
     
 }
 
--- web-api/routes/route.js
+++ web-api/routes/route.js
@@ -3,9 +3,11 @@ const pacienteController = require('../controllers/pacienteController')
 
 
 const router = express.Router();
-router.get('/api/getPacientes', pacienteController.getPacientes);
+router.get('/api/getPacientes', pacienteController.getPacientes); //http://localhost:4000/api/getPacientes
+router.get('/api/getPaciente/:Id_Paciente', pacienteController.getPaciente); //Se usa como ej: http://localhost:4000/api/getPaciente/1
+router.post('/api/addPaciente', pacienteController.addPaciente);
 /*router.get('/api/getPlayer/:id', playerController.getPlayer);
-router.post('/api/addPlayer', playerController.addPlayer);
+
 router.put('/api/updatePlayer/:id', playerController.updatePlayer);
 router.delete('/api/deletePlayer/:id' , playerController.deletePlayer);
 router.get('/api/getAvatars', avatarController.getAvatars);
